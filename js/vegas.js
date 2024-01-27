24763d646c9b9aaa3e125ba2c2b6e6ef1e837af0
keiMacAir <kyoheiogawa10@gmail.com>
2024-01-27 17:46:29 +0900

    top画面-修正-各メニュー(2)

--- js/vegas.js
+++ js/vegas.js
@@ -1,14 +1,17 @@
+
+// スライド設定
 setTimeout(function () {
   $("#mainimg").vegas({
     slides: [
-      { src: "./images/1.jpg" }, //1枚目の写真指定
-      { src: "./images/2.jpg" }, //2枚目の写真指定
-      { src: "./images/3.jpg" }, //3枚目の写真指定
+      { src: "./images/citys/city01.jpeg" }, //1枚目の写真指定   /Users/mactuve/dev/git/asterweb/images/citys/city01.jpeg
+      { src: "./images/citys/city02.jpeg" }, //2枚目の写真指定
+      { src: "./images/citys/city03.jpeg" }, //3枚目の写真指定
+      { src: "./images/citys/city04.jpeg" }, //3枚目の写真指定
     ],
     overlay: true, //写真上にドットを重ねる。不要ならこの１行を削除して下さい。
     transition: "blur", //https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
     animation: "kenburns", //https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
-    delay: 5000, //次の画像を表示するまでの時間
+    delay: 900, //次の画像を表示するまでの時間
     animationDuration: 20000, //アニメーション間の引き継ぎタイミング。
   });
-}, 3000); //3秒遅れてスライドショーがスタート
+}, 300); //0.3秒遅れてスライドショーがスタート
