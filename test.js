0994936f8864e2b1ed89985bf7c8db3a9ccf51ff
[key10] <[kyoheiogawa10@gmail.com]>
2024-02-05 18:51:03 +0900

    UI修正-各ページ画像置き換え中＋(ロゴアニメーション未完了)

--- test.js
+++ test.js
@@ -8,3 +8,22 @@
 
 
 // TODO　お店情報記載！＠＃＄％＾＆＊()＿＋{}L:"<>?"
+
+
+// TODO SNS連携(FB,X,Instagram,YouTube,Line)
+
+時間無制限　12,000円
+(ボトルキープ制)
+
+TAX 15%
+
+
+〒110-0005
+東京都台東区上野2-5-4
+サンシャインビルB1F G号
+
+
+
+
+<a href="menu.html">Menu<span>メニューのご紹介</span></a>
+
